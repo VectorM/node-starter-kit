d9084a111610839e33fb5b3ee2785d49cf03859c
VectorM <ua.itzakon@gmail.com>
2017-07-10 18:09:08 +0300

    rest begin

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('Hello world')
+import express, { Router } from 'express';
+
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+const bookRouter = Router();
+
+bookRouter.route('/Books')
+  .get((req,res) => {
+    const responeJson = {hello: "This is my api"};
+
+    res.json(responeJson)
+  })
+
+app.use('/api', bookRouter)
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my API')
+})
+
+app.listen(port, () => {
+  console.log(`Running on PORT: ${port}`);
+})
